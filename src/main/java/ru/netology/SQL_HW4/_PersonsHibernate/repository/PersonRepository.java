6d8162266fbe211dce255225bfbc994335988d4c
Ekaterina Kononova <ekaterina.panova@mail.ru>
2023-12-28 12:20:55 +0500

    HQL query

--- src/main/java/ru/netology/SQL_HW4/_PersonsHibernate/repository/PersonRepository.java
+++ src/main/java/ru/netology/SQL_HW4/_PersonsHibernate/repository/PersonRepository.java
@@ -10,22 +10,14 @@ import java.util.List;
 @Repository
 public class PersonRepository {
 
-//    @PersistenceContext  // многопоточный @Autowire
-//    private EntityManager entityManager;
     @PersistenceContext
-    private final EntityManager entityManager;
-
-    public PersonRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
+    // многопоточный @Autowire; @PersistenceContext умеет инжектировать только напрямую в поле, без конструктора
+    private EntityManager entityManager;
 
     public List<Persons> getPersonsByCity(String city) {
 
-        List<Persons> personsList = entityManager.createNativeQuery("select*  from netology.persons p", Persons.class).getResultList();
-
-        return personsList.stream()
-                .filter(x -> x.getCityOfLiving().equals(city))
-                .toList();
+        return entityManager.createQuery("SELECT p FROM Persons p WHERE p.cityOfLiving LIKE :requiredCity") // запрос на HQL (Hibernate Query Language)
+                .setParameter("requiredCity", city)
+                .getResultList();
     }
 }
